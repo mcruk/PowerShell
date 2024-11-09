9f0dcd1305d7beca9140ee86eea5ff55ff3abdd3
mcruk <14611257+mcruk@users.noreply.github.com>
2024-11-09 19:16:09 +0000

    Add files via upload

---
+++ Tools/Clear Cache And Temp/RunMe.bat
@@ -0,0 +1,2 @@
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"G:\Clear Cache And Temp\ClearCacheAndTemp.ps1"'"
