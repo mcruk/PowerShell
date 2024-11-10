dbfed169f3220b821e9f5bbae57e77757f690ff0
mcruk <14611257+mcruk@users.noreply.github.com>
2024-11-10 18:13:38 +0000

    Add files via upload

---
+++ Azure AD/Delete Stale Devices/DeleteStaleDevices.ps1
@@ -0,0 +1,9 @@
+﻿Install-Module AzureAD
+Connect-AzureAD 
+
+$dt = (Get-Date).AddDays(-60)
+$Devices = Get-AzureADDevice -All:$true | Where {($_.ApproximateLastLogonTimeStamp -le $dt) -and ($_.AccountEnabled -eq $false)}
+
+foreach ($Device in $Devices) {
+	Remove-AzureADDevice -ObjectId $Device.ObjectId
+}
