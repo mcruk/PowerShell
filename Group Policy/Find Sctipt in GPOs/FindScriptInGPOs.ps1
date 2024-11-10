616b1e63c11e2dfff036fe89209f12b98803c45f
mcruk <14611257+mcruk@users.noreply.github.com>
2024-11-10 18:00:22 +0000

    Add files via upload

---
+++ Group Policy/Find Sctipt in GPOs/FindScriptInGPOs.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'SilentlyContinue'
+$scriptName = Read-Host "Please enter the file to query e.g Test.ps1"
+
+# Get all the GPOs in the domain
+$gpos = Get-GPO -All
+
+$count = 1
+# Loop through each GPO and generate a report
+foreach ($gpo in $gpos) {
+
+     
+    $gpo.DisplayName = $gpo.DisplayName.replace(':','')
+    
+    $reportPath = "C:\GpoReport\$($gpo.DisplayName)_Report.html"
+    New-Item -Force -Path $reportPath | out-null
+    Get-GPOReport -Name $gpo.DisplayName -ReportType HTML -Path $reportPath
+
+    # Search the report for your script name
+    $matches = Select-String -Path $reportPath -Pattern $scriptName 
+
+    # If there is a match, print the GPO name and the path to the report
+    if ($matches){
+        Write-Output "The script '$scriptName' is being used in the GPO '$($gpo.DisplayName)'."
+        Write-Output "The report for this GPO can be found at '$reportPath'."
+        $count++
+    }else{
+
+        # Delete the report
+        Remove-Item $reportPath
+    
+    }
+
+}
+
+if($count -le 1){
+
+    write-host "Nothing to see here"
+
+}
+
+start-sleep -s 999
---
+++ Group Policy/Find Unlinked GPOs/FindAllUnlinkedGPOs.ps1
@@ -0,0 +1,18 @@
+﻿ Import-Module GroupPolicy
+ $Date = Get-Date -Format dd_MM_yyyy
+ $BackupDir = ".\Backup\$Date"
+## Creates a directory to store the GPO reports
+ if (-Not(Test-Path -Path $BackupDir))  {
+   New-Item -ItemType Directory $BackupDir -Force
+ }
+# Get all GPOs with the gpo report type as XML and also look for the section  in the xml report.
+# Consider only the GPOs that doesnt have  section.
+ Get-GPO -All | Where-Object { $_ | Get-GPOReport -ReportType XML | Select-String -NotMatch "<LinksTo>" } | ForEach-Object {
+   # Backup the GPO, HTML report and saving the GPO details to text file are optional.
+   Backup-GPO -Name $_.DisplayName -Path $BackupDir
+    # Run the report and save as an HTML report to disk
+   Get-GPOReport -Name $_.DisplayName -ReportType Html -Path "$BackupDir\$($_.DisplayName).html"
+   # Create and append to a text file called UnlinkedGPOs.txt in the backup folder that
+   # contains each GPO object that Get-GPO returns
+   $_ | Select-Object * | Out-File "$BackupDir\UnLinkedGPOs.txt" -Append
+ }
